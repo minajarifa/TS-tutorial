91d92189de0f7a0035821a274451f10d69705776
Your Name <minajmiha02@gmail.com>
2025-04-30 20:24:41 +0600

    function signature

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,16 @@
 "use strict";
 let userInfo1;
 let userInfo2;
+let userInfo3;
 userInfo1 = () => {
     console.log("Hi, I am Arifa Islam");
 };
 userInfo2 = (name) => {
     console.log(`Hi, I am ${name}`);
 };
+userInfo3 = (name, age) => {
+    return (`Hi, I am ${name},${age}`);
+};
 userInfo1();
 userInfo2("Arifa the boro Apu");
+userInfo3("Arifa the boro Apu", 20);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 let userInfo1:()=>void;
 let userInfo2:(name:string)=>void;
+let userInfo3:(name:string,age:number)=>string;
 userInfo1=()=>{
 console.log("Hi, I am Arifa Islam")
 }
 userInfo2=(name:string)=>{
 console.log(`Hi, I am ${name}`)
 }
+userInfo3=(name:string,age:number)=>{
+return(`Hi, I am ${name},${age}`)
+}
 userInfo1()
 userInfo2("Arifa the boro Apu")
+userInfo3("Arifa the boro Apu",20)
