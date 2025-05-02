c441e41f03a25addcec0a2d768b0fd9a8591d481
Your Name <minajmiha02@gmail.com>
2025-05-02 15:05:58 +0600

    trpe

---
+++ dist/array.js
@@ -0,0 +1,14 @@
+"use strict";
+let users = [];
+let user1 = {
+    id: 1,
+    name: "arifa",
+    age: 20
+};
+let user2 = {
+    id: 1,
+    name: "ayma",
+    age: 27
+};
+users.push(user1, user2);
+console.log(users);
---
+++ src/array.ts
@@ -0,0 +1,21 @@
+let users=[];
+let user1:{
+    id:number,
+    name:string,
+    age:number
+}={
+    id:1,
+    name:"arifa",
+    age:20
+}
+let user2:{
+    id:number,
+    name:string,
+    age:number
+}={
+    id:1,
+    name:"ayma",
+    age:27
+}
+users.push(user1,user2);
+console.log(users)
